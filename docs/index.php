c436292362a3e10502ff98081c59b37fea56d4a5
ZerroTwo <75293608+Zeew00@users.noreply.github.com>
2022-12-10 12:22:35 -0500

    Fix stylesheets for documentation

--- docs/index.php
+++ docs/index.php
@@ -8,19 +8,19 @@
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
-	<link rel="shortcut icon" href="../../assets/images/favicon.ico" type="image/x-icon">
-	<link rel="icon" type="image/icon" href="../../assets/images/favicon.ico">
+	<link rel="shortcut icon" href="../assets/images/favicon.ico" type="image/x-icon">
+	<link rel="icon" type="image/icon" href="../assets/images/favicon.ico">
 	<meta content="Intertwined helps you set up your web servers securely, safely, and easily" name="description" />
 
-	<link rel="stylesheet" href="../../assets/css/index-bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../../assets/css/index-style.css" />
-    <link rel="stylesheet" type="text/css" href="../../assets/css/index-accordian.css" />
+	<link rel="stylesheet" href="../assets/css/index-bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/index-style.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/index-accordian.css" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800&display=swap">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800&display=swap">
 	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 	
-	<script src="../../assets/javascript/smoothscroll.js"></script>
+	<script src="../assets/javascript/smoothscroll.js"></script>
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
