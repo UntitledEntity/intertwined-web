821d7a9aa9f8c6079ec2ede31c34f41028eb28c5
Jacob Gluska <3e18amn20@protonmail.com>
2024-01-26 18:59:59 -0500

    Remove error reporting

--- dashboard/licenses/index.php
+++ dashboard/licenses/index.php
@@ -1,10 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
 require '../../includes/mysql_connect.php';
 include '../../includes/include_all.php';
 
